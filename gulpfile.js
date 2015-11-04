aa76dac3bc56f5d7a914b2f8367f20f157ae3004
Raptor831 <Raptor831@users.noreply.github.com>
2015-11-04 17:44:35 -0500

    SASS/Gulp/PHPStorm setup

--- gulpfile.js
+++ gulpfile.js
@@ -2,48 +2,65 @@
 var gulp = require('gulp');
 
 // Include Our Plugins
-var jshint = require('gulp-jshint');
-var sass = require('gulp-ruby-sass');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var prefix = require('gulp-autoprefixer');
-var livereload = require('gulp-livereload');
+var jshint     = require('gulp-jshint');
+var sass       = require('gulp-ruby-sass');
+var concat     = require('gulp-concat');
+var uglify     = require('gulp-uglify');
+var rename     = require('gulp-rename');
+var prefix     = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
+
+// set up browser-sync
+gulp.task('browser-sync', function(){
+  browserSync.init({
+    proxy: 'site.dev',
+    notify: false
+  });
+});
 
 // Lint Task
 gulp.task('lint', function() {
-    return gulp.src('js/src/*.js')
+    return gulp.src('js/main.js')
         .pipe(jshint())
-        .pipe(jshint.reporter('default'));
+        .pipe(jshint.reporter('default', {verbose: true}));
 });
 
 // Compile Our Sass
 gulp.task('sass', function() {
-    return gulp.src('css/**/*.scss')
-        .pipe(sass({style: 'compact'}))
-        .pipe(prefix("> 1%", "last 2 versions", "Firefox ESR", "Opera 12.1", "ie 8", "ie 9"))
-        .pipe(gulp.dest(''))
-        .pipe(livereload());
+    return sass('css/style.scss', { sourcemap: true, style: 'compact' })
+        .on('error', function (err) {
+          console.error('Error!', err.message);
+        })
+        .pipe(prefix("> 1%", "last 2 versions", "ie > 8"))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('.'))
+        .pipe(browserSync.stream());
 });
 
 // Concatenate & Minify JS
 gulp.task('scripts', function() {
-    return gulp.src('js/src/*.js')
-        .pipe(concat('main.js'))
-        .pipe(gulp.dest('js'))
+    return gulp.src('js/main.js')
         .pipe(rename('main.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest('js'))
+        .pipe(browserSync.stream());
+});
+
+// PHP Autorefresh
+gulp.task('refresh', function() {
+  return gulp.src('index.php')
         .pipe(livereload());
 });
 
+gulp.task('scripts-watch', ['lint', 'scripts'], browserSync.reload);
+
 // Watch Files For Changes
 gulp.task('watch', function() {
-    livereload.listen();
-    gulp.watch('js/src/*.js', ['lint', 'scripts']);
-    gulp.watch('css/**/*.scss', ['sass']);
-    gulp.watch('*.php', ['sass']);
+    gulp.watch('js/main.js', ['scripts-watch']);
+    gulp.watch('css/*.{scss,sass}', ['sass']);
+    gulp.watch('**/*.php', browserSync.reload);
 });
 
 // Default Task
-gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
+gulp.task('default', ['browser-sync', 'sass', 'scripts-watch', 'watch']);
